66bea1ac71938c5e366f21c667eb67d5414049db
Albert Shin <albshin07@gmail.com>
2020-07-03 13:41:33 -0400

    Replace Parcel 2 with Webpack (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import 'regenerator-runtime/runtime';
+import 'core-js/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode !== 'production';
+
+  const config = {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: {
+      main: './src/index.tsx',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[tj]sx?$/,
+          exclude: '/node_modules/',
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    modules: false,
+                    useBuiltIns: 'entry',
+                    corejs: 3,
+                    targets: '> 0.25%, not dead',
+                  },
+                ],
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+              ],
+              plugins: ['@emotion'],
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, '/src/index.html'),
+        chunks: ['main'],
+        inject: 'body',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, 'src', 'assets'),
+            to: 'assets/[path][name].[ext]',
+          },
+        ],
+      }),
+    ],
+  };
+
+  if (isDevelopment) {
+    config.devServer = {
+      contentBase: path.join(__dirname, 'dist'),
+      historyApiFallback: true,
+      port: 1234,
+    };
+  }
+
+  return config;
+};
