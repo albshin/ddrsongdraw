26f2cfe6538500bd26803472dfa1587e0d2c9f66
albshin <albshin07@gmail.com>
2020-07-04 15:47:38 -0400

    Fix scrollbar appearing on page when content did not overflow

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -13,7 +13,6 @@ interface ContentProps {
 export const Content = styled.main<ContentProps>`
   flex: 1;
   overflow-x: hidden;
-  overflow-y: scroll;
   margin-top: ${(props) => props.theme.navbarHeight};
   margin-bottom: ${(props) =>
     props.hasBottomNav ? props.theme.navbarHeight : ''};
