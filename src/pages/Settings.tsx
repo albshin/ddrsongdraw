242259ea49cd085240438979f2f11fd79852d67a
albshin <albshin07@gmail.com>
2020-08-05 21:44:00 -0400

    Fix Settings page back icon to navigate back

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { useCallback } from 'react';
+import { RouteComponentProps, navigate } from '@reach/router';
 import { IoMdArrowBack } from 'react-icons/io';
 
 import { Content, NavIcon } from '../components/shared';
 import Navbar from '../components/Navbar';
 import { ListGroup, ListGroupItem } from '../components/ListGroup';
 
-const Settings: React.FC<RouteComponentProps> = () => (
-  <>
-    <Navbar
-      title="Settings"
-      navLeft={
-        <NavIcon to="/">
-          <IoMdArrowBack />
-        </NavIcon>
-      }
-    />
-    <Content>
-      <ListGroup title="General">
-        <ListGroupItem link="about">About</ListGroupItem>
-      </ListGroup>
-    </Content>
-  </>
-);
+const Settings: React.FC<RouteComponentProps> = () => {
+  const handleOnClickBack = useCallback(() => {
+    navigate(-1);
+  }, []);
+
+  return (
+    <>
+      <Navbar
+        title="Settings"
+        navLeft={
+          <NavIcon onClick={handleOnClickBack}>
+            <IoMdArrowBack />
+          </NavIcon>
+        }
+      />
+      <Content>
+        <ListGroup title="General">
+          <ListGroupItem link="about">About</ListGroupItem>
+        </ListGroup>
+      </Content>
+    </>
+  );
+};
 
 export default Settings;
