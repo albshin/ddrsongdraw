8ba4ae7e865ac4e2bcad0cb5e99115cb19532229
albshin <albshin07@gmail.com>
2020-08-08 17:54:33 -0400

    Add service worker webpack plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import 'regenerator-runtime/runtime';
 import 'core-js/stable';
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,5 +8,6 @@ import App from './App';
 import { enableMapSet } from 'immer';
 
 enableMapSet();
+OfflinePluginRuntime.install();
 
 ReactDOM.render(<App />, document.getElementById('app'));
